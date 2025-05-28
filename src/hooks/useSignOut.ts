6d617699bb89fd065f9f32bf23a4c82fe6d6ae75
Hilaladiii <abdulhilal242@gmail.com>
2025-05-28 15:03:50 +0700

    feat : add sign out hook

--- src/hooks/useSignOut.ts
+++ src/hooks/useSignOut.ts
@@ -1,10 +1,14 @@
 import { signOut } from "@/services/auth";
+import { useQuizSetupStore } from "@/stores/useQuizSetupStore";
+import { useQuizStore } from "@/stores/useQuizStore";
 import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export const useSignOut = () => {
   const navigate = useNavigate();
+  const { resetQuiz } = useQuizStore();
+  const { resetSetup } = useQuizSetupStore();
   return useMutation({
     mutationFn: () => signOut(),
     onSuccess: (response) => {
@@ -12,6 +16,9 @@ export const useSignOut = () => {
         toast.error(response.error.message);
         return;
       }
+      resetSetup();
+      resetQuiz();
+      localStorage.removeItem("quiz-time-left");
       navigate("/auth/sign-in");
     },
     onError: (error) => {
