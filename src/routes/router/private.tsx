98f70fcf2bdb579a07824d1dfde45b78aeb2511f
Hilaladiii <abdulhilal242@gmail.com>
2025-05-28 15:08:50 +0700

    feat : add private route

--- src/routes/router/private.tsx
+++ src/routes/router/private.tsx
@@ -1,9 +1,25 @@
-import Home from "@/pages/home";
+import MainLayout from "@/components/layout/MainLayout";
+import Quiz from "@/pages/quiz";
+import Result from "@/pages/result";
+import Setup from "@/pages/setup";
 import type { RouteObject } from "react-router-dom";
 
 export const privateRoutes: RouteObject[] = [
   {
-    path: "/home",
-    element: <Home />,
+    Component: MainLayout,
+    children: [
+      {
+        path: "/setup",
+        element: <Setup />,
+      },
+      {
+        path: "/quiz",
+        element: <Quiz />,
+      },
+      {
+        path: "/result",
+        element: <Result />,
+      },
+    ],
   },
 ];
